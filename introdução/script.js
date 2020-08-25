8e78db271890d02a249dff241362a95bbda5ea30
Hyago Braga <40799862+bhyago@users.noreply.github.com>
2020-08-25 11:40:25 -0300

    condicionais e loops

---
+++ introdução/script.js
@@ -0,0 +1,11 @@
+let app = new Vue ({
+  el: '#app',
+  data: {
+    names: [
+      { name:'Hyago', lastName:'Mesquita', year: 23 },
+      { name:'Maria', lastName:'Souza', year: 50 },
+      { name:'João', lastName:'Silva', year: 50 },
+      { name:'Milto', lastName:'alves', year: 23 },
+    ],
+  } 
+})
