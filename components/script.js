f58d6bb2b450d67f55656353a13ae21e122b9000
Hyago Braga <40799862+bhyago@users.noreply.github.com>
2020-08-28 17:08:33 -0300

    estudo de componentes

--- components/script.js
+++ components/script.js
@@ -1,3 +1,29 @@
+Vue.component('money', {
+  props: ['value', 'prefix', 'showColor'],
+  templete: '<span :class="className">{{prefix}} {{formatedValue}}</span>',
+  computed: {
+    formatedValue: function() {
+      numbro.culture('pt-BR');
+      return numbro(this.value).format('0[.]0');
+    }
+  },
+  className: function() {
+    if(!this.showColor) {
+      return ''
+    }
+
+    if (this.value > 0) {
+      return 'is-positive';
+    }
+
+    else if (this.value < 0) {
+      return 'is-negtive';
+    }
+
+    return 'is-neutral'
+  },
+});
+
 let app = new Vue ({
   el: '#app',
   data: {
