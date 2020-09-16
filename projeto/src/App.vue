8fdb7fe1c125ce932cb288c6cccb6eda39419631
Hyago Braga <40799862+bhyago@users.noreply.github.com>
2020-09-16 18:16:01 -0300

    form

---
+++ projeto/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <router-view>
+          <SignIn />
+        </router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import SignIn from "./components/SignIn";
+
+export default {
+  name: "App",
+
+  components: {
+    SignIn,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ projeto/src/components/SignIn.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center pa-1 v-card v-sheet theme--light">
+    <div class="v-card__title justify-center display-1 mb-2">Bem vindo</div>
+    <div class="v-card__subtitle">Sign in to your accont</div>
+    <div class="v-card__text">
+      <v-form>
+        <v-text-field label="usuário" outlined></v-text-field>
+        <v-text-field label="senha" outlined></v-text-field>
+
+        <v-btn
+          x-large
+          color="sucess"
+          class="v-btn--block v-btn-contained mb-2 v-size--x--large primary"
+        >entrar</v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
